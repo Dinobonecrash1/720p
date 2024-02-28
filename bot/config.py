a767abfb0a4878302fac7e061826a785aac55e22
Siam <ssiam4235@gmail.com>
2024-02-29 03:18:15 +0600

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,35 @@
+from bot.get_cfg import get_config
+
+class Config(object):
+    # You can keep this default
+    SESSION_NAME = get_config("SESSION_NAME", "AHCompressorBot")
+    # AHCompressBot....
+    # sucks Dude
+    APP_ID = get_config("APP_ID","") # Updated with your API ID
+    API_HASH = get_config("API_HASH","")  # Updated with your API HASH
+    LOG_CHANNEL = get_config("LOG_CHANNEL","") # Updated with your log channel ID
+    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
+    # Get these values from my.telegram.org
+    AUTH_USERS = {5090651635}
+    # auth users jdk 
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN","")  # Updated with your bot token
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
+    # Telegram maximum file upload size
+    BOT_USERNAME = get_config("BOT_USERNAME","")  # Updated with your bot username
+    MAX_FILE_SIZE = 2097152000
+    TG_MAX_FILE_SIZE = 2097152000
+    FREE_USER_MAX_FILE_SIZE = 2097152000
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = get_config("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    # proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    HTTP_PROXY = get_config("HTTP_PROXY", None)
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
+    # add config vars for the display progress
+    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "■")
+    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "❏")
+    LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
+    # because, https://t.me/c/1494623325/5603
+    SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", True)
